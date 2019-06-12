1159f21bab144064bcd4feb1986eb901675c8dc3
dpopadic <dario.popadic@yahoo.com>
2019-06-12 22:52:46 +0100

    data.table intro finalized

--- data_table_overview.R
+++ data_table_overview.R
@@ -25,7 +25,54 @@ DT[c(2,2,3)]
 # select 2nd to last row..
 DT[2:.N]
 
+# j argument
+# ---
+# select columns..
+DT[,.(B, C)]
+# computing on columns..
+DT[,.(total=sum(A), avg=mean(C))]
+# recycling in j (similar to mutate)..
+DT[,.(B, C=sum(C))]
+DT[,.(B,val=A*C)]
+# throw anything in j..
+DT[,plot(A,C)]
+DT[, {
+  print(A)
+  hist(C)
+  NULL
+}]
+# return a column as vector..
+DT[,B]
 
+# by argument
+# ---
+# doing j by group..
+DT[, .(x1=sum(C), x2=mean(C)), by=.(B)]
+# function calls in by..
+DT[, .(x1=sum(C)), by=.(Grp=A%%2)]
+# grouping only by subset..
+DT[2:4, sum(C), by=A%%2]
+# remember: no need to use .() if only 1 argument in i & j!
+
+# some more basics..
+# ---
+DT<- data.table(iris)
+# for each Species, compute mean of Sepal.Length..
+DT[, mean(Sepal.Length), by=Species]
+# compute mean Sepal.Length, grouping by first letter of Species..
+DT[, mean(Sepal.Length), by=substr(Species,1,1)]
+# group the specimens by Sepal area (to the nearest 10 cm2) and count how many occur in each group..
+DT[, .N, by = 10 * round(Sepal.Length * Sepal.Width / 10)]
+# name the output columns `Area` and `Count`..
+DT[, .(Count=.N), by = .(Area=10 * round(Sepal.Length * Sepal.Width / 10))]
+# return multiple numbers in j..
+DT<- data.table(A = rep(letters[2:1], each = 4L), 
+                B = rep(1:4, each = 2L), 
+                C = sample(8))
+# new data.table DT2
+DT2<- DT[,.(C=cumsum(C)),by=.(A, B)]
+# last two values from C while you group by A..
+DT2[,.(C=tail(C,2)),by=A]
 
 
 
