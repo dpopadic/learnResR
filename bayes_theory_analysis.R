940aa62e6eec141e38abaf62d500b71f06ecb1ec
dpopadic <dario.popadic@yahoo.com>
2019-09-29 21:11:16 +0100

    framework for bayesian data analysis

---
+++ bayes_theory_analysis.R
@@ -0,0 +1,18 @@
+# BAYESIAN DATA ANALYSIS FUNDAMENTALS
+# -----------------------------------
+
+# 1. What is Bayesian Data Analysis
+
+# 2. How does Bayesian inference work
+
+# 3. Why use Bayesian Data Analysis
+
+# 4. Bayesian inference with Bayes' theorem
+
+# 5. More parameters, more data, and more Bayes
+
+
+
+
+
+
